cccbb40de90802be974ea2c6a2cd320ab2408957
Jens Breitbart <jbreitbart@gmail.com>
2015-11-27 01:29:35 +0100

    Added realloc to test.cpp.

--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
 #include <cstdlib>
+#include <cassert>
 
 int main() {
 	void *t = malloc(100);
+	assert(t != nullptr);
+
+	void *t_realloc = realloc(t, 100 * 1024);
+	assert(t_realloc != nullptr);
+	t = t_realloc;
+
 	free(t);
 	return 0;
 }
