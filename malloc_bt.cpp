1c95fcf09f3b6ac47d0c197c38ec0fe2a4386b71
Jens Breitbart <jbreitbart@gmail.com>
2015-11-27 01:42:06 +0100

    Added realloc and calloc support.
    Fixes #1.

--- malloc_bt.cpp
+++ malloc_bt.cpp
@@ -1,33 +1,53 @@
 #include "malloc.h"
 
 #include <cstdio>
+#include <cstring>
 
 extern "C" void *__libc_malloc(size_t size);
 extern "C" void *__libc_free(void *ptr);
+extern "C" void *__libc_realloc(void *ptr, size_t new_size);
 
 extern "C" void *malloc(size_t size) {
 	printf("malloc size: %zu\n", size);
 	const size_t new_size = size + sizeof(size_t);
-	char *temp = (char *)__libc_malloc(new_size);
-	*((size_t *)temp) = size;
-	return (void *)(temp + sizeof(size_t));
+	char *temp = static_cast<char *>(__libc_malloc(new_size));
+	*reinterpret_cast<size_t *>(temp) = size;
+	return static_cast<void *>(temp + sizeof(size_t));
 }
 
 extern "C" void free(void *ptr) {
 	if (ptr == 0) return;
 
-	char *n = (char *)ptr;
-	void *real_ptr = (void *)((char *)ptr - sizeof(size_t));
-	// get the size of the memory freed
+	volatile char *ptr_ch = static_cast<volatile char *>(ptr);
 
-	size_t *size_ptr = (size_t *)(n - sizeof(size_t));
-// write zeros
+	size_t *size_ptr = reinterpret_cast<size_t *>(const_cast<char *>(ptr_ch) - sizeof(size_t));
+// write zeros, do not use memset as it may be optimized away
 #pragma omp simd
-	for (size_t i = 0; i < *(size_ptr); ++i) {
-		n[i] = 0;
+	for (size_t i = 0; i < *size_ptr; ++i) {
+		ptr_ch[i] = 0;
 	}
 
 	*size_ptr = 0;
-	// call _free()
+	void *real_ptr = static_cast<void *>(const_cast<char *>(ptr_ch) - sizeof(size_t));
 	__libc_free(real_ptr);
+}
+
+extern "C" void *realloc(void *ptr, size_t new_size) {
+	printf("realloc %p - %zu\n", ptr, new_size);
+	if (new_size == 0) return nullptr;
+
+	void *temp = malloc(new_size);
+
+	size_t old_size = *(size_t *)(static_cast<char *>(ptr) - sizeof(size_t));
+	std::memcpy(temp, ptr, old_size);
+
+	free(ptr);
+	return temp;
+}
+
+extern "C" void *calloc(size_t num, size_t size) {
+	printf("calloc %zu - %zu\n", num, size);
+	void *ptr = malloc(num * size);
+	std::memset(ptr, 0, num * size);
+	return ptr;
 }
